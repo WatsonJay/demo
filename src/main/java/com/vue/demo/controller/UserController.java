4bcd91e386464bea9632a6b2eb73973cc7214d59
WatsonJay <446725026@qq.com>
2019-06-06 11:15:08 +0800

    add deleteUser

--- src/main/java/com/vue/demo/controller/UserController.java
+++ src/main/java/com/vue/demo/controller/UserController.java
@@ -28,16 +28,34 @@ public class UserController {
     @PostMapping("/submit")
     @CrossOrigin
     public Object submitUser(User user) {
-
         Map<String,Object> result = new HashMap<String, Object>();
-        if(user.getId()==0) {
-            userService.newp(user);
-        }else{
-            userService.update(user);
+        try {
+            if(user.getId()==0) {
+                userService.newp(user);
+            }else{
+                userService.update(user);
+            }
+            result.put("success", true);
+            return result;
         }
-        result.put("success", true);
-        result.put("msg", "登录成功");
-        result.put("token", "adminxxxx");
-        return result;
+        catch (Exception e){
+            result.put("success", false);
+            return result;
+        }
+    }
+
+    @PostMapping("/deleteUser")
+    @CrossOrigin
+    public Object deleteUser(int id){
+        Map<String,Object> result = new HashMap<String, Object>();
+        try{
+            userService.delete(id);
+            result.put("success", true);
+            return result;
+        }
+        catch (Exception e){
+            result.put("success", false);
+            return result;
+        }
     }
 }
